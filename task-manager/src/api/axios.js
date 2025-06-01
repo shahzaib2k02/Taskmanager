9d7c14424df4582e1ae5a50782298a03eae4791c
shahzaib2k02 <k213819@nu.edu.pk>
2025-06-01 11:43:36 +0500

    added axios

--- task-manager/src/api/axios.js
+++ task-manager/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // 🔁 Replace with your backend URL
+  baseURL: 'https://taskmanager-dl2q.onrender.com/api', // 🔁 Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
